57fa95a6890b08990fe1a0a3983298d8a3242b14
Armen Badalian <abadalia@synaptics.com>
2019-02-14 17:11:27 +0400

    Մանր-մունր 8

--- src/main/java/basic/parser/Builtins.java
+++ src/main/java/basic/parser/Builtins.java
@@ -14,14 +14,24 @@ public class Builtins
 {
     public Builtins()
     {
+    	String classpath = System.getProperty("java.class.path");
+		String[] classpathEntries = classpath.split(System.getProperty("path.separator"));
+        String rjarname = "basic-rt.jar";
+		for( String s : classpathEntries )
+			if(s.endsWith(rjarname)) {
+                rjarname = s;
+                break;
+            }
+        System.out.printf("------> %s\n", rjarname);
+
         try {
-            JarFile jar  = new JarFile("basic-rt.jar");
+            JarFile jar  = new JarFile(rjarname);
             Enumeration<JarEntry> entries = jar.entries();
             while( entries.hasMoreElements() ) {
                 JarEntry je = entries.nextElement();
                 if( !je.getName().endsWith(".class") )
                     continue;
-                ClassParser cps = new ClassParser("basic-rt.jar", je.getName());
+                ClassParser cps = new ClassParser(rjarname, je.getName());
                 JavaClass jcl = cps.parse();
 
                 System.out.println("Class: " + jcl.getClassName()); // D
